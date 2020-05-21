bcdbb6fbbf3597107d80d98b9bdad0b50fa55df6
Pkmans <60535510+Pkmans@users.noreply.github.com>
2020-05-20 22:26:59 -0700

    Added shooting out a hook
    Shoots out hook when right clicking,
    not grappling mechanic yet

---
+++ Run & Gun/Assets/Scripts/RopeScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeScript : MonoBehaviour
+{
+    public Vector2 destiny;
+
+    public float speed ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, destiny, speed);
+    }
+}
---
+++ Run & Gun/Assets/Scripts/shootHook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootHook : MonoBehaviour
+{
+    public GameObject hook;
+
+    private GameObject curHook;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Vector2 destiny = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            curHook = (GameObject) Instantiate(hook, transform.position, Quaternion.identity);
+            curHook.GetComponent<RopeScript>().destiny = destiny;
+
+        }
+    }
+}
