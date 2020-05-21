46f99b38d22e0a20795ab5dae1a3569db9ad3d3d
Pkmans <60535510+Pkmans@users.noreply.github.com>
2020-05-21 11:49:10 -0700

    Hook mechanic somewhat finished
    renders a line, swings like a rope, the physics isnt smooth at the bottom of the swing though

--- Run & Gun/Assets/Scripts/RopeScript.cs
+++ Run & Gun/Assets/Scripts/RopeScript.cs
@@ -7,16 +7,23 @@ public class RopeScript : MonoBehaviour
 
     public Vector2 destiny;
     public float speed;
+	public float distance = 1;
     public GameObject node;
+	public GameObject lastNode;
     public GameObject gun;
-    public GameObject go;
     private bool done = false;
-    
+    public LineRenderer lr;
+
+	int vertexCount = 2;
+	public List<GameObject> Nodes = new List<GameObject>();
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
+
+		lr = GetComponent<LineRenderer>();
         gun = GameObject.FindGameObjectWithTag("Gun");
+		lastNode = transform.gameObject;
+		Nodes.Add(transform.gameObject);
     }
 
     // Update is called once per frame
@@ -25,23 +32,53 @@ public class RopeScript : MonoBehaviour
         // hook moves to where the mouse pos when right mb was clicked
         transform.position = Vector2.MoveTowards(transform.position, destiny, speed);
 
-        if ((Vector2) transform.position == destiny)
+        if ((Vector2) transform.position != destiny)
         {
-            if (done == false)
-            {
-                CreateNode();
-                done = true;
-                go.GetComponent<HingeJoint2D>().connectedBody = gun.GetComponent<Rigidbody2D>();
+			if (Vector2.Distance (gun.transform.position, lastNode.transform.position) > distance) {
+				
+				CreateNode ();
+			
+			}
+	
+            
+        } else if (done == false) {
+				done = true;
+				while(Vector2.Distance (gun.transform.position, lastNode.transform.position) > distance) {	
+					CreateNode ();
+				}
+                lastNode.GetComponent<HingeJoint2D>().connectedBody = gun.GetComponent<Rigidbody2D>();
             }
-        }
+
+		RenderLine();
     }
+
+	void RenderLine() {
+		lr.positionCount = vertexCount;
+		
+		int i;
+		for (i = 0; i < Nodes.Count; i++) {
+
+			lr.SetPosition (i, Nodes [i].transform.position);
+
+		}
+
+		lr.SetPosition (i, gun.transform.position);
+	}
+
 
     void CreateNode()
     {
-        Vector2 posCreate = new Vector2(gun.transform.position.x + (float) 0.3, gun.transform.position.y);
-        go = (GameObject) Instantiate(node, posCreate, Quaternion.identity);
+        Vector2 posCreate = gun.transform.position - lastNode.transform.position;
+        posCreate.Normalize();
+		posCreate *= distance;
+		posCreate += (Vector2)lastNode.transform.position;
+		GameObject go = (GameObject) Instantiate(node, posCreate, Quaternion.identity);
         
-        transform.gameObject.GetComponent<HingeJoint2D>().connectedBody = go.gameObject.GetComponent<Rigidbody2D>();
- 
+		go.transform.SetParent(transform);
+
+        lastNode.GetComponent<HingeJoint2D>().connectedBody = go.gameObject.GetComponent<Rigidbody2D>();
+ 		lastNode = go;
+		Nodes.Add(lastNode);
+		vertexCount++;
     }
 }
