6e9c848e6f61fcc13559242e87b9c5e39eb811cf
Pkmans <60535510+Pkmans@users.noreply.github.com>
2020-06-22 13:43:06 -0700

    FINISHED main menu

---
+++ Run & Gun/Assets/Scripts/ButtonAnimation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonAnimation : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Animator animator;
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        animator.SetBool("Selected", true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        animator.SetBool("Selected", false);
+    }
+}
--- Run & Gun/Assets/Scripts/MainMenu.cs
+++ Run & Gun/Assets/Scripts/MainMenu.cs
@@ -7,6 +7,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void startGame() {
+        
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
