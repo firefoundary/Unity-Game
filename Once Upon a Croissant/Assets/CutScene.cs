e35dc9bba70fe6c9bc24e964205793f95d9402db
Pkmans <60535510+Pkmans@users.noreply.github.com>
2020-07-07 15:54:38 -0700

    Added opening cutscene

---
+++ Once Upon a Croissant/Assets/CutScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
+
+
+
+public class CutScene : MonoBehaviour
+{
+    public PlayableAsset cutscene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float duration = (float) cutscene.duration;
+		Invoke("LoadNextLevel", duration);
+    }
+
+
+    public void LoadNextLevel()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
