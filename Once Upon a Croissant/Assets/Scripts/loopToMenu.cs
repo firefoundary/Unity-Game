bc9c35705a32e8618a175a3710f071120ff94554
Pkmans <60535510+Pkmans@users.noreply.github.com>
2020-07-20 19:41:46 -0700

    Updated Dialogues, more refining

---
+++ Once Upon a Croissant/Assets/Scripts/loopToMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class loopToMenu : MonoBehaviour
+{
+    public GameObject text, text1, text2;
+    public GameObject fadeOut, endCredits, blackBackground;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(loopMenu());
+    }
+
+    IEnumerator loopMenu() {
+        //dialogue
+        yield return new WaitForSeconds(2f);
+        text.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        text.SetActive(false);
+        text1.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        text1.SetActive(false);
+        text2.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        text2.SetActive(false);
+        yield return new WaitForSeconds(3f);
+
+        //transition to end credits
+        fadeOut.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        blackBackground.SetActive(true);
+        endCredits.SetActive(true);
+        yield return new WaitForSeconds(12f);
+
+        //back to menu
+        SceneManager.LoadScene(0);
+    }
+}
